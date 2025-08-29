5a1f587febb1c04be3ac26960860bbf400327c13
Saii Sarraan <147138065+2211043070020@users.noreply.github.com>
2025-08-29 16:17:59 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,1 +1,9 @@
+# display.py - Functions to show information to the user
+
+from utils import format_date
+
+def show_events(events):
+    print("\nUpcoming Events:\n")
+    for idx, event in enumerate(events, 1):
+        print(f"{idx}. {event['name']} on {format_date(event['date'])} (ID: {event['id']})")
 
